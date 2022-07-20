e508d1673d0edb831f09679e3d68d9d264a60c29
Sebastian Silva <sebastian@fuentelibre.org>
2022-07-20 10:46:58 -0400

    Fix para mock

--- unique-note/src/firebase/__mocks__/firebaseConfig.js
+++ unique-note/src/firebase/__mocks__/firebaseConfig.js
@@ -1,7 +1,4 @@
-export const getDocs = jest.fn(() =>  {
-    console.log('probando getDocs')
-    return Promise.resolve([])
-});
+export const getDocs = () => Promise.resolve([]);
 export const query = jest.fn({});
 export const where = jest.fn({});
 export const collection = jest.fn({});
